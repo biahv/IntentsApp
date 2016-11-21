4c7345b9b1e36b9323e90dff4977ee6f752ee07a
Aluno <beatriz.hviana@gmail.com>
2016-11-21 20:45:43 -0200

    Criando

--- app/src/main/java/br/edu/iff/pooa20162/intents/MainActivity.java
+++ app/src/main/java/br/edu/iff/pooa20162/intents/MainActivity.java
@@ -1,7 +1,12 @@
 package br.edu.iff.pooa20162.intents;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText url = (EditText) findViewById(R.id.edURL);
+        Button ok = (Button) findViewById(R.id.btURL);
+
+        ok.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View view){
+                Uri uri = Uri.parse(url.getText().toString());
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
     }
 }
